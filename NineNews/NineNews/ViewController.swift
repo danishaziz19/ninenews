6db79d848d612d1b0a6a19eda7254a06af9c663d
Danish Aziz <danishaziz1989@gmail.com>
2019-10-18 16:36:41 +1100

    setup project

---
+++ NineNews/NineNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NineNews
+//
+//  Created by Danish Aziz on 18/10/19.
+//  Copyright © 2019 Danish Aziz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
